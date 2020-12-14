956910f264eb51000b3d416e099aeb060de41807
Long Do <longdo@dobtech.vn>
2020-12-14 21:34:07 +0700

    layout transaction card

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,19 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((transaction) {
               return Card(
-                child: Text(transaction.title),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(transaction.amount.toString()),
+                    ),
+                    Column(
+                      children: [
+                        Text(transaction.title),
+                        Text(transaction.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
