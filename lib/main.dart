9ff892ff921342d3db99986f3a7ddbdfc40a74ee
Long Do <longdo@dobtech.vn>
2020-12-19 10:52:32 +0700

    show no transaction image when there are no transaction

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
+    /* Transaction(
       id: 't1',
       title: 'New Shoe',
       amount: 69.99,
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Weekly Groceries',
       amount: 16.53,
       date: DateTime.now(),
-    ),
+    ), */
   ];
 
   void _addNewTransaction({String title, double amount}) {
