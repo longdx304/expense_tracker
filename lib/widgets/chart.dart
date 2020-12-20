313a8e45a6affaff4051294a52396df022f9ca85
Long Do <longdo@dobtech.vn>
2020-12-20 13:47:14 +0700

    fix display issues

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -26,7 +26,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay)[0],
         'amount': totalSum,
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
